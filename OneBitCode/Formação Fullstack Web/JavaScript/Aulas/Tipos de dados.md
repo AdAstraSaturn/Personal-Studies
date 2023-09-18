e411c4cab957565af3558b5b822d65caae83ae11
Eduardo Maciel Bueno <contatoeduardomacielbueno@gmail.com>
2023-09-17 22:18:53 -0300

    feat: adicionada aula sobre tipos de dados

---
+++ OneBitCode/Formação Fullstack Web/JavaScript/Aulas/Tipos de dados.md
@@ -0,0 +1,13 @@
+# Tipos de dados
+
+Dados podem ser representados de diferentes formas.
+
+## Tipos primitivos
+
+-   number e bigint
+-   string
+    -
+-   boolean
+-   null e undefined
+
+## Objetos e funções
---
+++ OneBitCode/Formação Fullstack Web/JavaScript/Praticas/tipos-de-dados.js
@@ -0,0 +1,7 @@
+console.log(typeof 10); // Number
+console.log(typeof 10n); // BigInt
+console.log(typeof "10"); // String
+console.log(typeof true); // Boolean
+console.log(typeof null); // object || null
+console.log(typeof undefined); // undefined
+console.log(typeof Date); // function
