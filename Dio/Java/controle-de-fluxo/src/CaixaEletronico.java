ba236080b9ac12690b271ff4e67ad46dcb37f617
Eduardo Maciel Bueno <contatoeduardomacielbueno@gmail.com>
2023-10-03 22:47:17 -0300

    JAVA - Controle de fluxo

--- Dio/Java/controle-de-fluxo/src/CaixaEletronico.java
+++ Dio/Java/controle-de-fluxo/src/CaixaEletronico.java
@@ -1,10 +1,14 @@
 public class CaixaEletronico {
   public static void main(String[] args) {
     double saldo = 25.0;
-    double valorSolicitado = 20.0;
+    double valorSolicitado = 29.0;
 
+    System.out.println(saldo <= valorSolicitado);
+    
     if (valorSolicitado < saldo)
       saldo -= valorSolicitado;
+    else
+      System.out.println("Saldo insuficiente!");
 
     System.out.println(saldo);
   }
