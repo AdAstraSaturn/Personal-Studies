d35e6c9c32c5ec73a140cabbb7474ecccefa6362
Eduardo Maciel Bueno <contatoeduardomacielbueno@gmail.com>
2023-10-04 22:54:17 -0300

    JAVA: Added more lessons related to java syntax

---
+++ Dio/Java/controle-de-fluxo/src/Contador.java
@@ -0,0 +1,8 @@
+public class Contador {
+  public static void main(String[] args) {
+    
+    for (int contagem = 0; contagem <= 20; contagem++) {
+      System.out.println(contagem);
+    }
+  }
+}
---
+++ Dio/Java/controle-de-fluxo/src/ExemploFor.java
@@ -0,0 +1,9 @@
+public class ExemploFor {
+  public static void main(String[] args) {
+    String alunos[] = { "Eduardo", "Ihully", "Josias" };
+
+    for (int index = 0; index < alunos.length; index++) {
+      System.out.println("O aluno do índice " + index + " é " + alunos[index]);
+    }
+  }
+}
