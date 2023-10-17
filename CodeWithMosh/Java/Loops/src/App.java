93001cd3a71baa08e772d2b76506f0ee31cc742c
Eduardo Maciel Bueno <contatoeduardomacielbueno@gmail.com>
2023-10-16 22:15:03 -0300

    feat: added codeWithMosh related content.

---
+++ CodeWithMosh/Java/Loops/src/App.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        while (!input.equals("quit")) {
+            System.out.print("Input: ");
+            input = scanner.next().toLowerCase();
+            System.out.println(input);
+        }
+    }
+}
